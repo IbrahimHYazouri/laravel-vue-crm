5683ef6e88135530ff17a84acad0e7add3ad3ad6
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-07-12 11:04:33 +0200

    build 🧩: update authentication handling process

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -21,13 +21,13 @@ const sidebarOpen = ref(false);
               </div>
               <h3 class="text-indigo-600 font-bold text-lg">Laravel</h3>
           </div>
-          <div class="ms-auto">
+          <div class="ms-auto" v-if="$page.props.auth.user">
             <Dropdown align="right" width="48">
                 <template #trigger>
                     <button
                         class="inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none"
                     >
-                        {{ $page.props.auth.user.name }}
+                        <span class="bg-gray-400 text-zinc-900 p-1 rounded-lg">{{ $page.props.auth.user.first_name[0] + $page.props.auth.user.last_name[0] }}</span>
                         <svg
                             class="ms-1 h-4 w-4" fill="currentColor" viewBox="0 0 20 20">
                             <path
@@ -49,7 +49,7 @@ const sidebarOpen = ref(false);
         <div class="flex">
             <aside
                 :class="{'w-64': sidebarOpen, 'w-15': !sidebarOpen}"
-                class="fixed bottom-0 top-[58px] bg-gray-200 p-4 overflow-y-auto"
+                class="fixed bottom-0 top-[58px] bg-gray-200 p-4 overflow-y-auto flex flex-col gap-3"
             >
                 <Link
                   :href="route('dashboard')"
@@ -61,15 +61,21 @@ const sidebarOpen = ref(false);
                     </svg>
                     <p v-show="sidebarOpen">Dashboard</p>
                 </Link>
+                <Link
+                    :href="route('users.index')"
+                    class="flex items-center gap-3 text-gray-700 hover:bg-zinc-700 hover:text-white rounded transition-colors duration-150"
+                    :class="{'bg-zinc-700 text-white': route().current('users.index'), 'px-3 py-1': sidebarOpen, 'p-2 justify-center': !sidebarOpen}"
+                >
+                    <svg class="size-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M144 0a80 80 0 1 1 0 160a80 80 0 1 1 0-160m368 0a80 80 0 1 1 0 160a80 80 0 1 1 0-160M0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96H21.3C9.6 320 0 310.4 0 298.7M405.3 320h-.7c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7c58.9 0 106.7 47.8 106.7 106.7c0 11.8-9.6 21.3-21.3 21.3H405.4zM224 224a96 96 0 1 1 192 0a96 96 0 1 1-192 0m-96 261.3c0-73.6 59.7-133.3 133.3-133.3h117.3c73.7 0 133.4 59.7 133.4 133.3c0 14.7-11.9 26.7-26.7 26.7H154.6c-14.7 0-26.7-11.9-26.7-26.7z"/></svg>
+                    <p v-show="sidebarOpen">Users</p>
+                </Link>
             </aside>
 
             <main
-                class="flex-1 ml-64 py-6"
-                :class="{'ml-64': sidebarOpen, 'ml-10': !sidebarOpen}"
+                class="flex-1"
+                :class="{'ml-64': sidebarOpen, 'ml-14': !sidebarOpen}"
             >
-                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <slot />
-                </div>
+                <slot />
             </main>
         </div>
     </div>
---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Users" />
+    <AuthenticatedLayout>
+        <div class="m-4 rounded-md bg-gray-200">
+            <p class="p-2 border-b">Users</p>
+            <div class="p-2 bg-white flex flex-col gap-5">
+                <button class="w-fit px-2 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700">+ Add User</button>
+                <hr />
+                <button class="w-fit px-2 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700 border-b">Show deleted users</button>
+            </div>
+            <div class="overflow-x-auto">
+                <table class="w-full whitespace-nowrap">
+                    <tr class="text-left text-xs text-zinc-500 font-light uppercase">
+                        <th class="p-2">name</th>
+                        <th class="p-2">email</th>
+                        <th class="p-2">role</th>
+                        <th class="p-2">address</th>
+                        <th class="p-2">phone</th>
+                        <th class="p-2">terms accepted</th>
+                        <th class="p-2">actions</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
