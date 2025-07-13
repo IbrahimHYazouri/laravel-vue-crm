77e29802755d9fdade76b930e093d2b959b61149
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-07-13 12:16:35 +0200

    build 💡: handle clinet update & delete

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
 use App\Models\Client;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -27,5 +29,26 @@ class ClientController extends Controller
         Client::create($request->validated());
 
         return redirect()->route('clients.index')->with('status', __('Client successfully created.'));
+    }
+
+    public function edit(Client $client): Response
+    {
+        return Inertia::render('Clients/Edit', ['client' => $client]);
+    }
+
+    public function update(UpdateClientRequest $request, Client $client): RedirectResponse
+    {
+        $client->update($request->validated());
+
+        return redirect()->route('clients.index')->with('status', __('Client successfully updated.'));
+    }
+
+    public function destroy(Client $client): RedirectResponse
+    {
+        Gate::authorize('delete');
+
+        $client->delete();
+
+        return redirect()->route('clients.index')->with('status', __('Client successfully deleted.'));
     }
 }
