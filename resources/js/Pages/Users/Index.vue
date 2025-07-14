a776a4199902d41ffaddd3bbd98def92ba116ed1
Ibrahim H. Yazouri <ibrahimhyaz@gmail.com>
2025-07-14 11:13:19 +0200

    build 🧩: store project media and info

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -72,7 +72,7 @@ defineProps<{
                     </tr>
                     </thead>
                     <tbody class="bg-white  divide-y divide-gray-200 ">
-                    <tr v-if="users" v-for="user in users.data" class="hover:bg-gray-50 ">
+                    <tr v-if="users" v-for="user in users.data" class="hover:bg-gray-50">
                         <td class="px-6 py-4 whitespace-nowrap">
                             <div class="text-sm text-gray-900">{{ user.full_name }}</div>
                         </td>
